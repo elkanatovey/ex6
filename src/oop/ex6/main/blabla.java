a8094191871ccdbf7b1f8bd75fb5976dbd76ea11
shellygamllielly <shellygamlliely@gmail.com>
2018-06-21 21:44:51 +0300

    Merge branch 'master' of https://github.com/shellygamlielly/ex6
    # Conflicts:
    #	src/oop/ex6/main/regexManager.java

--- src/oop/ex6/main/blabla.java
+++ src/oop/ex6/main/blabla.java
@@ -15,15 +15,23 @@ public class blabla {
         String bla = "   int b=1; ";
         String a = "    int a;";
         String b = "   a=b;";
-//        String c = "   int a=b , c=d;";
+//        String cond = "true";
+        String c = "   int a=b , c=d;";
         try {
             HashMap<String,GlobalVariable> globals = new HashMap<>();
             regexManager.isValidGlobalVariable(bla, globals);
             regexManager.isValidGlobalVariable(a, globals);
-//            regexManager.isValidGlobalVariable( c, globals);
-//            System.out.println(globals.isEmpty());
+            regexManager.isValidGlobalVariable( c, globals);
+            System.out.println(globals.isEmpty());
         } catch (CompileErrorException e) {
             System.out.println(222);
         }
+
+//        String[] listcond = cond.split("([|]{2}|[&]{2})");
+//        for (int i = 0; i < listcond.length; i++) {
+//            System.out.println(listcond[i]);
+//        }
+
+
     }
 }
