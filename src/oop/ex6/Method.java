b71cd8d43bdfc0daea2c417b30f627074d0694c7
elkanatovey <elkanatovey@gmail.com>
2018-06-21 16:19:20 +0300

    method work

--- src/oop/ex6/Method.java
+++ src/oop/ex6/Method.java
@@ -5,22 +5,37 @@ import java.util.LinkedList;
 
 public class Method {
 
-    private HashMap<String, GlobalVariable> variablesInScope;
+    private HashMap<String, LocalVariable> variablesInScope;
 
     private String methodName;
 
     private LinkedList<String> linesToRead;
+
+    private HashMap<String, Method> methodNames;
 
     /**
      * Constructor for a method
      * @param variablesInScope
      * @param methodName
      */
-    public Method(HashMap<String, GlobalVariable> variablesInScope, String methodName, LinkedList<String>
-            linesToRead){
+    public Method(HashMap<String, LocalVariable> variablesInScope, String methodName, LinkedList<String>
+            linesToRead, HashMap<String, Method> legalMethods){
         this.variablesInScope = variablesInScope;
         this.methodName = methodName;
         this.linesToRead = linesToRead;
+        this.methodNames = legalMethods;
+    }
+
+    /**
+     * Check if a method has been declared
+     * @param methodName
+     * @param legalMethods
+     * @return
+     */
+    public static boolean isLegalMethod(String methodName, HashMap<String,Method> legalMethods){
+        if(legalMethods.containsKey(methodName))
+            return true;
+        return false;
     }
 
 
