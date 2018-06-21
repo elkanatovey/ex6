e8b5462a475a6d6c429410691e1075fcc56c9f39
elkanatovey <elkanatovey@gmail.com>
2018-06-21 15:27:02 +0300

    continued work on method

--- src/oop/ex6/Method.java
+++ src/oop/ex6/Method.java
@@ -1,13 +1,29 @@
 package oop.ex6;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Method {
-    Method(String typeOfVar, String isFinal,String name ){
+
+    private HashMap<String, GlobalVariable> variablesInScope;
+
+    private String methodName;
+
+    private String linesToRead
+
+    /**
+     * Constructor for a method
+     * @param variablesInScope
+     * @param methodName
+     */
+    public Method(HashMap<String, GlobalVariable> variablesInScope, String methodName, LinkedList<String>
+            linesToRead){
+
         //if and while are inside method
         //if final the variable cant be changed inside the method
         //handle recursive calls
         //different name to method
         //no need to declare method inside another (calling a method only inside another method)
         //aaa
-
     }
 }
