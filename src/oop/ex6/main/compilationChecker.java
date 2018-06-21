d3d42ccf646a13643c0bb301e25285131b8e05f6
elkanatovey <elkanatovey@gmail.com>
2018-06-21 18:03:42 +0300

    continued work on compile checker

--- src/oop/ex6/main/compilationChecker.java
+++ src/oop/ex6/main/compilationChecker.java
@@ -2,6 +2,7 @@ package oop.ex6.main;
 
 import oop.ex6.CompileErrorException;
 import oop.ex6.GlobalVariable;
+import oop.ex6.Method;
 
 import java.io.IOException;
 import java.io.LineNumberReader;
@@ -10,6 +11,14 @@ import java.util.LinkedList;
 
 public class compilationChecker {
 
+
+    private static final int GLOBAL_SCOPE = 0;
+    /**
+     * receive a file to read and check if it compiles
+     * @param fileToRead
+     * @throws CompileErrorException
+     * @throws IOException
+     */
     public static void compileCheck(LineNumberReader fileToRead) throws CompileErrorException, IOException {
         HashMap<String,GlobalVariable> globals = new HashMap<>();
         String currentLine;
@@ -24,29 +33,40 @@ public class compilationChecker {
         } while (currentLine != null);
     }
 
+    /*
+    check type of current line
+     */
     private static int basicLegalCheck(String currentLine, int scope, HashMap<String, GlobalVariable> globals)
             throws CompileErrorException {
         currentLine = currentLine.trim();
         char endOfLine = currentLine.charAt(currentLine.length() - 1);
         switch (endOfLine) {
             case ';':
-                if (scope == 0) {
+                if (scope == GLOBAL_SCOPE) {
                     regexManager.isValidGlobalVariable(currentLine, globals);
-//                    for (GlobalVariable variable : variables) {
-//                        if (globals.containsKey(variable.getName()))
-//                            throw new CompileErrorException();
-//                        globals.put(variable.getName(),variable);
-//                    }
                     return 0;
                 }
+                regexManager.
             case '{':
+                if (scope==GLOBAL_SCOPE) {
+                    regexManager.isValidParameterVariable(currentLine,);
+                    regexManager.
+                }
+                regexManager.isIfStatement(currentLine);
+                regexManager.isWhileStatement(currentLine);
                 return 1;
             case '}':
+                if (scope==GLOBAL_SCOPE+1)
+                    //check for return
                 return -1;
             default: {
                 throw new CompileErrorException();
             }
         }
+    }
+
+    private static void checkMethods(HashMap<String, GlobalVariable>, HashMap<String,Method>){
+
     }
 }
 
