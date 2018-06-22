8ad730a8979aecc1d02133d98c6a623c6addeb7e
elkanatovey <40407298+elkanatovey@users.noreply.github.com>
2018-06-22 19:39:21 +0300

    checks variable calls

--- src/oop/ex6/dataStructures/LocalVariable.java
+++ src/oop/ex6/dataStructures/LocalVariable.java
@@ -3,13 +3,9 @@ package oop.ex6.dataStructures;
 
 public class LocalVariable extends Variable {
 
-    private int scope;
 
     public LocalVariable(String type, boolean initialized, boolean isFinal, String name){
         super(type, initialized,isFinal, name);
     }
 
-    public int getScope() {
-        return scope;
-    }
 }
